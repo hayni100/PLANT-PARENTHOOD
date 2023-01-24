c8747e7eff15b6d788426886c79c23d2ca1868e1
Haein Kim <hayni100@gmail.com>
2023-01-23 21:27:07 -0800

    added backend post

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,10 @@
-const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const myPlantsRoutes = require('./myPlantsRoutes');
-const plantSearch = require('./plantSearch');
+const router = require("express").Router();
+const userRoutes = require("./userRoutes");
+const myPlantsRoutes = require("./myPlantsRoutes");
+const plantSearch = require("./plantSearch");
 
-router.use('/users', userRoutes);
-router.use('/myPlants', myPlantsRoutes);
-router.use('/search',plantSearch);
+router.use("/myPlants", myPlantsRoutes);
+router.use("/users", userRoutes);
+router.use("/search", plantSearch);
 
 module.exports = router;
--- controllers/api/myPlantsRoutes.js
+++ controllers/api/myPlantsRoutes.js
@@ -1,21 +1,49 @@
-const router = require('express').Router();
-const { Plant } = require('../../models');
-const withAuth = require('../../utils/auth');
+const router = require("express").Router();
+const { Plant } = require("../../models");
+const withAuth = require("../../utils/auth");
 
-router.post('/', withAuth, async (req, res) => {
+router.post("/", withAuth, async (req, res) => {
   try {
     const newPlant = await Plant.create({
-      ...req.body,
+  
+      name: req.body.common[0],
+      maxTemp: req.body.tempmax.fahrenheit,
+      minTemp: req.body.tempmin.fahrenheit,
+      idealLight: req.body.ideallight,
+      toleratedLight: req.body.toleratedlight,
+      watering: req.body.watering,
       user_id: req.session.user_id,
     });
-
+    console.log("@@@@@@ HIT ADD PLANT POST");
     res.status(200).json(newPlant);
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
-router.delete('/:id', withAuth, async (req, res) => {
+//NEED TO USE ABOVE CODE BLOCK//
+// router.post("/", async (req, res) => {
+//   try {
+//     const newPlant = await Plant.create({
+//       // ...req.body,
+//       // user_id: req.session.user_id,
+
+//       name: req.body.common[0],
+//       maxTemp: req.body.tempmax.fahrenheit,
+//       minTemp: req.body.tempmin.fahrenheit,
+//       idealLight: req.body.ideallight,
+//       toleratedLight: req.body.toleratedlight,
+//       watering: req.body.watering,
+//       user_id: 1,
+//     });
+//     console.log("@@@@@@ HIT ADD PLANT POST");
+//     res.status(200).json(newPlant);
+//   } catch (err) {
+//     res.status(400).json(err);
+//   }
+// });
+
+router.delete("/:id", withAuth, async (req, res) => {
   try {
     const myPlantData = await Plant.destroy({
       where: {
@@ -25,7 +53,7 @@ router.delete('/:id', withAuth, async (req, res) => {
     });
 
     if (!myPlantData) {
-      res.status(404).json({ message: 'No plants found with this id!' });
+      res.status(404).json({ message: "No plants found with this id!" });
       return;
     }
 
